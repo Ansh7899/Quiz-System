a421a9b8e83bde4ba6aa9ee2710238e94aaf0673
Ansh7899 <63894658+ansh7899@users.noreply.github.com>
2020-06-10 13:06:22 +0530

    A non web based quiz system with java swing code for the frames and wampserver as its back-end

---
+++ src/quiz/Quiz.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quiz;
+
+/**
+ *
+ * @author anshh
+ */
+public class Quiz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Login l =new Login();
+         l.setVisible(true);
+         l.setTitle("QUIZ SYSTEM");
+    }
+    
+}
---
+++ src/quiz/User3.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quiz;
+
+/**
+ *
+ * @author anshh
+ */
+public class User3 {
+     private String name;
+     private int score;
+    
+    public User3(String name, int Score)
+    {
+         this.name=name;
+         this.score=Score;
+         
+                 }
+        public String getname()
+        {
+            return name;
+        }
+        public int getscore()
+        {
+            return score;
+        }
+}
